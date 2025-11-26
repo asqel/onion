caf985abc9fa06e72d49e449ffe3127a58b26112
asqel <axel.lleres@gmail.com>
2025-11-26 16:11:57 +0100

    oops

---
+++ include/parser.h
@@ -0,0 +1,29 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "onion.h"
+#include <stddef.h>
+
+enum {
+	TOKEN_INVALID,
+	TOKEN_NUM,
+	TOKEN_STR,
+	TOKEN_BOOL,
+	TOKEN_CHAR
+};
+
+typedef struct token_t {
+	char type;
+	union {
+		double num;
+		char *str;
+		char c;
+	} val;
+	int line;
+} token_t;
+
+token_t *_I_lexe_json(char *text, int *len);
+json_value_t *_I_json_parse(char *text);
+void _I_json_free_tokens(token_t *tokens, size_t len);
+
+#endif
